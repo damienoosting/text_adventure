a4ee9fcd743fc862faf7fe7cad72b685150a8700
damien <537949@student.saxion.nl>
2023-11-24 12:19:55 +0100

    init
    sandbox

---
+++ Sandbox/Sandbox/Sandbox/src/Application.java
@@ -0,0 +1,22 @@
+import nl.saxion.app.CsvReader;
+import nl.saxion.app.SaxionApp;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Application implements Runnable {
+    ArrayList<Sport> exercises = new ArrayList<Sport>();
+
+    public static void main(String[] args) {
+        SaxionApp.start(new Application(), 1024, 768);
+    }
+
+    public void run() {
+
+    }
+}
+
+
+
+
+
---
+++ Sandbox/Sandbox/Sandbox/src/Sport.java
@@ -0,0 +1,8 @@
+public class Sport {
+
+    String name = "";
+    int times = -1;
+    int addedwheight = -1;
+    String muscle = "";
+    boolean beginner = false;
+}
