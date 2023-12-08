7b57fc414593ff79b969955dbd6404ae02109551
damien <537949@student.saxion.nl>
2023-12-08 13:50:17 +0100

    werkend menu met 1e story board! 2.0

--- Sandbox/Sandbox/Sandbox/src/Application.java
+++ Sandbox/Sandbox/Sandbox/src/Application.java
@@ -15,18 +15,29 @@ public class Application implements Runnable {
         //menu
         boolean menu = true;
 
-            menu();
-            char select = SaxionApp.readChar();
-            if (select == '2'){
-                menu = false;
-            } else if (select == '1') {
+        menu();
+        char select = SaxionApp.readChar();
+        if (select == '2') {
+            menu = false;
+        } else if (select == '1') {
+            SaxionApp.clear();
+            board1();
+            char option = SaxionApp.readChar();
+            if (option == '1') {
                 SaxionApp.clear();
-                board1();
-                
+                board2();
+
+            } else {
+                SaxionApp.clear();
+                board3();
+
             }
 
         }
-    public void menu(){
+
+    }
+
+    public void menu() {
         String text = "cyberpunk";
         String menu1 = "1.start";
         String menu2 = "2.quit";
@@ -47,11 +58,12 @@ public class Application implements Runnable {
             x = x + 50;
         }
         SaxionApp.sleep(0.5);
-        SaxionApp.drawText("1. start", 250,300,50);
+        SaxionApp.drawText("1. start", 250, 300, 50);
         SaxionApp.sleep(0.5);
-        SaxionApp.drawText("2. quit", 250,400,50);
+        SaxionApp.drawText("2. quit", 250, 400, 50);
 
     }
+
     public void board1() {
         String text = """
                 In the dystopian expanse of Neo-Tokyo, Cipher, a skilled hacker,
@@ -69,7 +81,7 @@ public class Application implements Runnable {
 
             try {
 
-                Thread.sleep(SaxionApp.getRandomValueBetween(0, 800));//pause between letterprint
+                Thread.sleep(SaxionApp.getRandomValueBetween(0, 80));//pause between letterprint
 
             } catch (InterruptedException ex) {
                 Thread.currentThread().interrupt();
@@ -84,22 +96,48 @@ public class Application implements Runnable {
         SaxionApp.printLine("1. story option a");
         SaxionApp.printLine();
         SaxionApp.printLine("2. story option b");
-        char option = SaxionApp.readChar();
+    }
+    public void board2() {
+        String text = """
+                you chose 1! well done""";
+        int i = 0;
+        int x = 250;
 
-        if (option == '1'){
-            SaxionApp.clear();
-            SaxionApp.printLine("skibidi fortnite");
+        while (i < text.length()) {
+            SaxionApp.print(String.valueOf(text.charAt(i)));
 
+            try {
+
+                Thread.sleep(SaxionApp.getRandomValueBetween(100, 300));//0.5s pause between characters
+
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            i++;
+            x = x + 50;
         }
-        else{
-            SaxionApp.clear();
-            SaxionApp.printLine("fortnite!!!!");
+
+    }
+    public void board3(){
+        String text = """
+                    you chose 2! well done.
+                    """;
+        int i = 0;
+        int x = 250;
+
+        while (i < text.length()) {
+            SaxionApp.print(String.valueOf(text.charAt(i)));
+
+            try {
+
+                Thread.sleep(SaxionApp.getRandomValueBetween(100, 300));//0.5s pause between characters
+
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            i++;
+            x = x + 50;
         }
 
     }
 }
-
-
-
-
-
